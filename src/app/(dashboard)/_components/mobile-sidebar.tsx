e3bf03d02ae86e06d7b8ce033719e3e7a4f3621e
Matheus <matheuschmidt.b@gmail.com>
2024-07-16 09:47:26 -0300

    feat: update mobile sidebar adding title and description

--- src/app/(dashboard)/_components/mobile-sidebar.tsx
+++ src/app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,4 +1,10 @@
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { DialogDescription } from "@radix-ui/react-dialog";
 import { MenuIcon } from "lucide-react";
 import Sidebar from "./sidebar";
 
@@ -9,6 +15,10 @@ const MobileSidebar = () => {
         <MenuIcon />
       </SheetTrigger>
       <SheetContent side="left" className="bg-white p-0">
+        <SheetTitle className="hidden">Menu</SheetTitle>
+        <DialogDescription className="hidden">
+          Use the sidebar to navigate to different pages
+        </DialogDescription>
         <Sidebar />
       </SheetContent>
     </Sheet>
